0d3d1d46e3958e47a206d8b27eba2e1f408eeea2
denisdelaki <denisdelaki@gmail.com>
2022-09-29 22:24:00 +0300

    Done.

--- db/migrate/20210509111524_create_reviews.rb
+++ db/migrate/20210509111524_create_reviews.rb
@@ -5,7 +5,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
       t.string :comment
       t.integer :rating
       t.belongs_to :dog_house, null: false, foreign_key: true
-
+      
       t.timestamps
     end
   end
